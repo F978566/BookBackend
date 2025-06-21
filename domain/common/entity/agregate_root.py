d4d11e04e5bb9196b094fe5579c0829c71eae4b2
Fedos <fedor.potolokov2000@gmail.com>
2025-06-21 04:19:25 +0300

    create abstract entity

---
+++ domain/common/entity/agregate_root.py
@@ -0,0 +1,23 @@
+from abc import ABC
+from dataclasses import dataclass, field
+
+from event.event import Event
+from entity import Entity
+
+
+@dataclass
+class AgregateRoot(Entity, ABC):
+    _events: list[Event] = field(default_factory=list[Event])
+    
+    def record_event(self, event: Event) -> None:
+        self._events.append(event)
+    
+    def get_events(self) -> list[Event]:
+        return self._events
+    
+    def clear_events(self) -> None:
+        self._events.clear()
+    
+    def pull_events(self) -> list[Event]:
+        return [self._events.pop() for _ in range(len(self._events))]
+    
